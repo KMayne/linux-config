ea0194408b637381e7a8a50bc79a5028438b9f78
Kian Mayne <github@kianmayne.co.uk>
2017-07-31 14:41:54 +0100

    Improved setup script
    * Setup script now saves old config
    * Also fixed bug with $DIR getting by specifying bash as the interpreter

--- setup.sh
+++ setup.sh
@@ -1,12 +1,19 @@
+#!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-ln -s $DIR/zshrc ~/.zshrc
+# Save old configuration
+mkdir .old-config
+mv .zshrc .old-config 2&>/dev/null
+mv .vim .old-config 2&>/dev/null
+mv .vimrc .old-config 2&>/dev/null
+mv .ssh/config .old-config 2&>/dev/null
+mv .tmux.conf .old-config 2&>/dev/null
+mv .gitconfig .old-config 2&>/dev/null
 
+# Create symlinks for new config
+ln -s $DIR/zshrc ~/.zshrc
 ln -s -d $DIR/vim ~/.vim
 ln -s $DIR/vim/vimrc ~/.vimrc
-
 ln -s $DIR/ssh_config ~/.ssh/config
-
 ln -s $DIR/tmux.conf ~/.tmux.conf
-
 ln -s $DIR/gitconfig ~/.gitconfig
