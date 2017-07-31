05709799fe12dc7918cde058f9686da9ca60a8a7
Kian Mayne <git@kianmayne.co.uk>
2017-07-31 14:58:58 +0100

    Improved setup script: does more with fewer errors
    * Moving old config now works correctly
    * Creates the .ssh directory if it doesn't already exist
    * Creates the zsh_custom file to squelch errors from ZSH

--- setup.sh
+++ setup.sh
@@ -2,21 +2,28 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Save old configuration
+mv .old-config .old-config-tmp 2>/dev/null
 mkdir .old-config
-mv .zshrc .old-config 2&>/dev/null
-mv .vim .old-config 2&>/dev/null
-mv .vimrc .old-config 2&>/dev/null
-mv .ssh/config .old-config 2&>/dev/null
-mv .tmux.conf .old-config 2&>/dev/null
-mv .gitconfig .old-config 2&>/dev/null
+mv .old-config-tmp .old-config/ 2>/dev/null
+mv ~/.zshrc .old-config 2>/dev/null
+mv ~/.vim .old-config 2>/dev/null
+mv ~/.vimrc .old-config 2>/dev/null
+mv ~/.ssh/config .old-config 2>/dev/null
+mv ~/.tmux.conf .old-config 2>/dev/null
+mv ~/.gitconfig .old-config 2>/dev/null
 
 # Create symlinks for new config
 ln -s $DIR/zshrc ~/.zshrc
 ln -s -d $DIR/vim ~/.vim
 ln -s $DIR/vim/vimrc ~/.vimrc
+mkdir ~/.ssh 2>/dev/null
 ln -s $DIR/ssh_config ~/.ssh/config
 ln -s $DIR/tmux.conf ~/.tmux.conf
 ln -s $DIR/gitconfig ~/.gitconfig
 
 # Install Vim plugins
 vim +PluginInstall +qall
+
+# Ensure custom file exists
+touch ~/.zsh_custom
+
